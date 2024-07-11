f4b501055ba6b5bcf59b917aabace3956d37ba35
torres-jpw <torres.jpw@gmail.com>
2024-07-11 15:46:23 -0300

    fix errors

--- 02-node/03-express.js
+++ 02-node/03-express.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express();
-const ditto = require("./pokemon/ditto.json");
+const express = require('express')
+const app = express()
+const ditto = require('./pokemon/ditto.json')
 
-app.disable("x-powered-by");
+app.disable('x-powered-by')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json())
 // app.use((req, res, next) => {
 //   if (req.method !== "POST") return next();
 //   if (req.headers["content-type"] !== "application/json") return next();
@@ -26,20 +26,18 @@ app.use(express.json());
 //   });
 // });
 
-
-
-app.get("/pokemon/ditto", (req, res) => {
+app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
-});
+})
 
 app.post('/pokemon', (req, res) => {
   res.status(201).json(req.body)
 })
 
 app.use((req, res) => {
-  res.status(404).send("<h1>404 Not Found</h1>");
-});
+  res.status(404).send('<h1>404 Not Found</h1>')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port http://localhost:${PORT}`);
-});
+  console.log(`Server is listening on port http://localhost:${PORT}`)
+})
