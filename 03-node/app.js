82c43390759875c3a3cef591104012af2bbcffe6
pnauj <torres.jpw@gmail.com>
2024-07-11 21:38:41 -0300

    add get and post

--- 03-node/app.js
+++ 03-node/app.js
@@ -2,21 +2,30 @@ const express = require('express')
 const movies = require('./movies.json')
 
 const app = express()
+app.use(express.json())
 
 app.disable('x-powered-by')
 
 app.get('/movies', (req, res) => {
-  res.json(movies)
+  const { genre } = req.query
+
+  if (genre) {
+    const filterdMovies = movies.filter(movie => movie.genre.some(g => g.toLocaleLowerCase() === genre.toLocaleLowerCase()))
+    return res.json(filterdMovies)
+  }
 })
 
 app.get('/movies/:id', (req, res) => {
-  const id = req.params.id
+  const { id } = req.params
   const movie = movies.find(movie => movie.id === id)
-  if (movie) {
-    res.json(movie)
-  } else {
-    res.status(404).json({ message: 'Movie not found' })
-  }
+
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const { title, year, director, duration, poster, genre, rate } = req.body
 })
 
 const PORT = process.env.PORT ?? 3000
@@ -25,4 +34,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`)
 })
 
-// 00:20:00 movies.json
+// 00:34:00 movies.json
